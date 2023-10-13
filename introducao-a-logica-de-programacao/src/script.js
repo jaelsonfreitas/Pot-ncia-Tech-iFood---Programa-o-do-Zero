08447f44f6336406418ee3ccdf941b887e419c9f
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-13 20:59:52 -0300

    Estrutura do nosso programa.

--- introducao-a-logica-de-programacao/src/script.js
+++ introducao-a-logica-de-programacao/src/script.js
@@ -1,5 +1,10 @@
+//saida
 console.log("Digite o nome do seu jogador")
 
+// declarando a variavel.
 let nickname = "Jaelson top"
 
+//Concatenando uma mensagem fixa + uma variavel.
 console.log("Bem Vindo" + nickname)
+
+console.log(nickname + "Entrou no servidor ") 
