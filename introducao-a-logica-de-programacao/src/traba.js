dba9a4328e396500c5cfde16c636424580bd1d76
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-13 23:25:31 -0300

    Entendendo Strings,Numbers e Booleans.

---
+++ introducao-a-logica-de-programacao/src/traba.js
@@ -0,0 +1,12 @@
+// pokemon
+
+let nomePokemon = "Pikachu"
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+let pokemonSexo = 'M'
+let selecionavel = true 
+
+//Tipos de Variaveis
+// Strings são variaveis que armazenam texto.
+// Number são variaveis que armazenam numeros.
+// Boolean são variaveis lógicas que armazenam valores de Verdadeiro ou Falo (true/false).
