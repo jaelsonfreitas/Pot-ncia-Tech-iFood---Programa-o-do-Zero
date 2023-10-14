7f251f23aa66c21880a00debcb2f7dc2f17c0c9b
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-14 20:49:13 -0300

    Comparaç

---
+++ Explorando-operadores/Trabalhando-com-operadores-de-comparacao/script.js
@@ -0,0 +1,24 @@
+// Operadores relacionais ou operadores de comparação. == , != , >, <, >=, <= . 
+
+// Guardar o valor em uma variável de resultado TRUE?False
+let marca = "Apple"
+let resultado = marca !== "Samsung"
+console.log("Esta comparação é " + resultado + " pois " + marca + "é diferente de Samsung.")
+
+let number1 = 5
+let numer2 = 10
+console.log(number1 > numer2)
+
+let num1 = 1
+let num2 = 10
+ console.log("Este resultado é: " + (num1 > num2) + " pois " + num1 + " é menor que " + num2)
+ console.log("Este resultado é: " + (num1 < num2) + " pois " + num1 + " é menor que " + num2)
+
+// = é atribuição
+// == é para comparar o valor
+// === serve para comparar o valor e o formato do conteúdo.
+// !== é diferente ?
+// > é maior?
+// < é menor?
+// >= é maior ou igual?
+// <= é menor ou igual?
