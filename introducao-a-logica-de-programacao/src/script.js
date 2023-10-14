c27951df667824f824a4734689fb339dd44cc8be
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-13 21:11:50 -0300

    Diferença entre constante e variáveis.

--- introducao-a-logica-de-programacao/src/script.js
+++ introducao-a-logica-de-programacao/src/script.js
@@ -1,10 +1,9 @@
+// notificação pop up do programa.
+
+const notificacao = "Pokemon Go diz: "
+
 //saida
-console.log("Digite o nome do seu jogador")
+console.log(notificacao + "tem um novo pokemon na região")
+console.log(notificacao + "Você foi derrotado por um líder supremo.")
 
-// declarando a variavel.
-let nickname = "Jaelson top"
-
-//Concatenando uma mensagem fixa + uma variavel.
-console.log("Bem Vindo" + nickname)
-
-console.log(nickname + "Entrou no servidor ") 
+// uma constante nao muda o seu valor, podemos utilizar para nomes que nao mudam e se repetem muitas vezes no programa. Já uma variavel ;e possivel mudar seu valor no decorrer do programa.
