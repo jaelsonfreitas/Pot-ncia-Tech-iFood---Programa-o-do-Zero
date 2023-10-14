039fbb032c5420b02e32b8df52e9e5c151eb1cf8
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-14 10:49:42 -0300

    vantagens de usar vetores.

--- introducao-a-logica-de-programacao/src/vetores-e-matrizes.js
+++ introducao-a-logica-de-programacao/src/vetores-e-matrizes.js
@@ -1,6 +1,9 @@
-
 let pokemon = ["Pikachu", "charmander", "Bulbasaur"]
 
 console.log(pokemon[0])
 
+pokemon.shift()
+pokemon.pop()
 // em quase todas as linguagens de programaçòes as posições começam no 0. 
+// para descobrir mais sobre todas as opçoes de vetores consulte a documentação oficial. 
+// Quando quisermos guardar coleçoes de coisas parecidas use vetores.
