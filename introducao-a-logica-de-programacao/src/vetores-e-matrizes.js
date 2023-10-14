3e26089c947bc110310b267fb4e188ee093725af
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-14 11:09:31 -0300

    Trabalhando com matrizes.

--- introducao-a-logica-de-programacao/src/vetores-e-matrizes.js
+++ introducao-a-logica-de-programacao/src/vetores-e-matrizes.js
@@ -1,4 +1,10 @@
 let pokemon = ["Pikachu", "charmander", "Bulbasaur"]
+let timePokemon = [
+    ["Pikachu", "M", "level 1"],
+    ["charmander", "F", "level2"]
+]
+
+console.log(" O Pokemon "+ timePokemon[1][0] + " é do sexo " + timePokemon[1][1] + " e está no nível" + timePokemon[1][2] )
 
 console.log(pokemon[0])
 
