a85d7c77bc2bdcd5b794bcd9f00474ee113196f4
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-14 14:54:54 -0300

    operadores matematicos.

---
+++ Explorando-operadores/trabalhando-com-operadores-aritmeticos/index.js
@@ -0,0 +1,25 @@
+let idade = 30
+console.log(idade + 6)
+idade = 30 - 6
+console.log(idade)
+idade = 30 * 6
+console.log(idade)
+idade = 30 / 6
+console.log(idade)
+
+let primeiroNum = 1022
+let segundoNum = 22
+console.log(primeiroNum - segundoNum)
+
+let precoProd = 100.99
+let valorComTaxa = precoProd * 2
+console.log(valorComTaxa)
+
+let multiplicador = 4
+let multiplicando = 12
+let produto= multiplicador * multiplicando
+console.log('O resultado da multiplicação de ' + multiplicador + ' e ' + multiplicando + ' é igual á ' + produto)
+// utilizo let somente na primeira vez que atribuo a variavel.
+
+let notaDoMercado = 50
+console.log(notaDoMercado / 2)
