701dbce6ab60833a4d4eec9a4d2f43bbdd881872
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-13 21:24:59 -0300

    Colocando conhecimento na pratica com exemplo da cozinha da minha avó.

---
+++ introducao-a-logica-de-programacao/src/vovo.js
@@ -0,0 +1,9 @@
+let poteCafe = "Café Caboclo."
+let acucar = "Açucar Alvoradas."
+let bolacha = "Piraque Maltado."
+const mensagemDaVovo = "No armário da cozinha da minha avó tem: "
+
+console.log( mensagemDaVovo + poteCafe + " " + acucar + " " + bolacha)
+poteCafe = "Café Três Corações."
+
+console.log(mensagemDaVovo + poteCafe + " " + acucar + " " + bolacha)
