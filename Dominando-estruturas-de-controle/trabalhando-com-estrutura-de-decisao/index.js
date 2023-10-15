e47dd48324126ecd3e145e2e94da503eb01d06c9
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-15 15:27:41 -0300

    Switch Case.

---
+++ Dominando-estruturas-de-controle/trabalhando-com-estrutura-de-decisao/index.js
@@ -0,0 +1,15 @@
+// estrutura de decisão 
+// switch/case/break/default
+​
+let fruta = "Banana"
+​
+switch(fruta){
+    case "Laranja": 
+    console.log("Suco de Laranja")
+    
+    case "Banana":
+    console.log("Vitamina de Banana")
+    
+    case "Maça":
+    console.log("Suco de maça")
+}
