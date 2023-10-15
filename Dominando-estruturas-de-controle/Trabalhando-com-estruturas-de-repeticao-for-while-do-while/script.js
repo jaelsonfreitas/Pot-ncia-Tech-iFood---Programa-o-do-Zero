3859ec21387da03a2745328f1eec60dc0195241f
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-15 16:05:10 -0300

    Cuidados com for.

--- Dominando-estruturas-de-controle/Trabalhando-com-estruturas-de-repeticao-for-while-do-while/script.js
+++ Dominando-estruturas-de-controle/Trabalhando-com-estruturas-de-repeticao-for-while-do-while/script.js
@@ -4,4 +4,14 @@ for (let contador = 0; contador < 4; contador++){
     console.log(contador)
     console.log("Aumentar contador em mais um.")
     }
-    
+
+    // for 
+//neste caso tenho um personagem que a cada vez que ele toma um ponto de vida a sua vida aumenta.
+
+let pontosDeVida = 0
+for (let i = 0; i < 10; i++){
+	pontosDeVida += 1
+    console.log("Tomou poção magina " + i)
+}
+
+console.log(pontosDeVida + " Totais")
