8f1ff1f8e7657e599aee3938f2ff1f22cbd7205b
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-15 14:28:14 -0300

    Else.

---
+++ Dominando-estruturas-de-controle/trabalhando-com-estruturas-condicionais-if-else/index.js
@@ -0,0 +1,21 @@
+let ehLigado = false
+
+if(ehLigado) {
+	console.log("Executou comando")
+}
+
+//permite se o código tome um caminho se algo for verdade.
+
+// else
+
+let possuiOvos = false
+let itensComprados = ""
+if(possuiOvos){
+	itensComprados = "Leite"
+}
+else{
+	console.log("passou na sessão congelados")
+    itensComprados = "Lasanha Congelada"
+}
+
+console.log("itens comprado:" + itensComprados)
