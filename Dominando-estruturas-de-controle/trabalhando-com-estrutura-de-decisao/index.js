d80a5810addffd1cc714cd0fe774288f19ed740d
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-15 15:31:10 -0300

    Break

--- Dominando-estruturas-de-controle/trabalhando-com-estrutura-de-decisao/index.js
+++ Dominando-estruturas-de-controle/trabalhando-com-estrutura-de-decisao/index.js
@@ -6,10 +6,14 @@ let fruta = "Banana"
 switch(fruta){
     case "Laranja": 
     console.log("Suco de Laranja")
-    
+    break
+
     case "Banana":
     console.log("Vitamina de Banana")
-    
+    break 
+
     case "Maça":
     console.log("Suco de maça")
+    break
+    
 }
