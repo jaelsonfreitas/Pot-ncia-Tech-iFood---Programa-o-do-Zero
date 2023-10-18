e7a4809f6aef82c2a213eb76cf6be8636fb6156b
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-17 22:19:43 -0300

    Aula 2.

--- trabalhando-com-funcoes/oque-sao-e-como-criar/aula.js
+++ trabalhando-com-funcoes/oque-sao-e-como-criar/aula.js
@@ -11,3 +11,36 @@ function egetarPao(){
 	console.log("Egetando pão pronto")
     console.log("Pronto.")
     }
+
+    // funções nao devem iniciar com números.
+// Evite nome com espaço, use padrao camelCase
+// Funçoes são ações, coloque o nome claro do que esta mesma faz.
+// Sempre que abrir uma chave coloque um novo nivel de identacão.
+mainSaveData()
+
+function mainSaveData(){
+	getData()
+    checkValues()
+    sendToDataBase()
+}
+
+function enviarEmail(){}
+function salvarNoBancoDeDado(){}
+
+
+
+//Função que recebe todos os dados.
+function getData(){
+// lógica implentada aqui
+	console.log("Pegando dados de usuários")
+}
+
+function checkValues(){
+	console.log("Validando dados")
+}
+
+function sendToDataBase(){
+	console.log("Cadastrando Dados")
+}
+
+// Pela boa prática cada função deve fazer uma única coisa.
