343ffac79f9d7f3c48169383a9250d46735d89e9
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-17 22:06:46 -0300

    Adicionando primeira aula e criando pastas.

---
+++ trabalhando-com-funcoes/oque-sao-e-como-criar/aula.js
@@ -0,0 +1,13 @@
+// para chamar um função digito o nome dela seguido de ()
+torrar()
+
+function torrar(){
+//tudo que estiver dentro da chaves é o escopo da função.
+	console.log("Torrando o pão. em 3. 2. 1.")
+    egetarPao()
+}
+
+function egetarPao(){
+	console.log("Egetando pão pronto")
+    console.log("Pronto.")
+    }
