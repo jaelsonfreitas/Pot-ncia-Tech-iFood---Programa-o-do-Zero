7e9a8abbb7ce78605206eaa2f065874a89bd16b6
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-19 22:33:32 -0300

    Como Fazer.

---
+++ trabalhando-com-funcoes/funcoes-com-retorno/script.js
@@ -0,0 +1,10 @@
+
+let resultado = soma(5, 5)
+
+console.log("O resultado desta funcão é: " + resultado);
+function soma(numA, numB) {
+    let somatorio = numA + numB 
+    return somatorio
+}
+
+// tambem posso eliminar a variavel somatorio, e nunca ter dois valores no mesmo return
