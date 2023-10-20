39da7db674afbe6cc33c73d61cf1a870b6afa07d
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-19 21:24:01 -0300

    Aula 1, trabalhando com parâmetros.

---
+++ trabalhando-com-funcoes/funcoes-com-parametros/script.js
@@ -0,0 +1,9 @@
+torrar("Pão de forma.")
+torrar("Pão Integral.")
+​
+function torrar(pao) {
+    //escopo da nossa funcão.
+    console.log("Torrada feita com: " + pao)
+}
+​
+// geralmente passamos parametros quando queremos que uma função tenha resultados diferente.
