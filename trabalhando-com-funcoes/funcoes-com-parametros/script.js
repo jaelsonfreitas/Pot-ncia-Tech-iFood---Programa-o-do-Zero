8a4f0bf9312b9527dc444917aff24ee47c5485a5
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-19 21:57:25 -0300

    Interpolação.

--- trabalhando-com-funcoes/funcoes-com-parametros/script.js
+++ trabalhando-com-funcoes/funcoes-com-parametros/script.js
@@ -19,3 +19,19 @@ function torrar(pao, nome){
 	console.log("Torrada feita com: " + pao)
     console.log("Ela é um pedido de:" + nome)
 }
+
+// Podemos usar parâmetros Genéricos.
+torrar("Pão de Forma", "Jaélson", 15.99)
+
+function torrar(pao, nome = "Consumidor Final", valor){
+	console.log("Torrada feita com: " + pao)
+    console.log("Ela é um pedido de:" + nome)
+    console.log("Com valor total de R$:" + valor)
+}
+
+// Interpolação
+createStringConnection("db_products", "Jaélson", "1112211")
+
+function createStringConnection (databaseName, user, pass){
+	console.log(`connect:DBCONNECT;user=${user};pass=${pass};initial_database=${databaseName}`)
+    }
