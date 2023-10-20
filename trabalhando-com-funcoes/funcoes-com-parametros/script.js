72d7b519e92f8404de96386ad8054d56061c9884
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-19 21:32:53 -0300

    Escopo de variáveis e diversos parâmetros.

--- trabalhando-com-funcoes/funcoes-com-parametros/script.js
+++ trabalhando-com-funcoes/funcoes-com-parametros/script.js
@@ -7,3 +7,15 @@ function torrar(pao) {
 }
 ​
 // geralmente passamos parametros quando queremos que uma função tenha resultados diferente.
+
+// escopo de variáves sempre complica os dev.
+
+
+// não conseguimos chamar uma variável que só existe dentro do escopo. Se usar var antes de uma variável ela ira existir de maneira Global, porém não é recomendado.
+
+torrar("Pão de Forma", "Jaélson")
+
+function torrar(pao, nome){
+	console.log("Torrada feita com: " + pao)
+    console.log("Ela é um pedido de:" + nome)
+}
