c712aa1f49a7fbacb7f3c768203a01215dddb33d
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-21 12:02:32 -0300

    Variavel para calcular diferenca entre vitoria e derrota.

--- trabalhando-com-funcoes/desafio-de-projeto/projeto.js
+++ trabalhando-com-funcoes/desafio-de-projeto/projeto.js
@@ -3,4 +3,7 @@ function levelCalculator (victories, defeats) {
 }
 // primeiro criei a função com dois parâmetros ja estabelecidos.
 
- 
+let winBalance = victories - defeats;
+
+// neste trecho a variável winBalance irá armazenar a diferença entre a vitoria e derrota do jogador.
+
