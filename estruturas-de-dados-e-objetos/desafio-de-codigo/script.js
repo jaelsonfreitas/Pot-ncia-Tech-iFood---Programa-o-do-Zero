1f7913c8a03242af3638c4fbcb3104e1a99c8c2e
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-26 23:09:24 -0300

    Definindo ataques.

--- estruturas-de-dados-e-objetos/desafio-de-codigo/script.js
+++ estruturas-de-dados-e-objetos/desafio-de-codigo/script.js
@@ -6,3 +6,21 @@ class Heroi {
         this.idade = idade; // Define a propriedade "idade" do objeto Heroi
         this.tipo = tipo;   // Define a propriedade "tipo" do objeto Heroi
     }
+
+    // aqui vamos ver como realizar os ataques, vou usar estrutura de repetição.
+
+    atacar(){
+        let ataque; // é a variável para amazenar o tipo de ataque. 
+        switch (this.tipo) { // a partir daqui vamos verificar o tipo de heroi.
+            case "mago":
+                ataque = "usou magia"; // Se for um mago, define o ataque como "usou magia"
+                break;
+            case "guerreiro":
+                ataque = "usou espada"; // Se for um guerreiro, define o ataque como "usou espada"
+                break;
+            case "monge":
+                ataque = "usou artes marciais"; // Se for um monge, define o ataque como "usou artes marciais"
+                break;
+            case "ninja":
+                ataque = "usou shuriken"; // Se for um ninja, define o ataque como "usou shuriken"
+                break;
