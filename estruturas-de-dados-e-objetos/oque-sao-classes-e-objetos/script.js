04a40bdbe5e8423288a2fcb580fef6f65b06f49f
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-26 21:20:29 -0300

    Trabalhando com métodos e propriedades.

--- estruturas-de-dados-e-objetos/oque-sao-classes-e-objetos/script.js
+++ estruturas-de-dados-e-objetos/oque-sao-classes-e-objetos/script.js
@@ -3,7 +3,11 @@ class formaDeBolo {
         this.saborDaMassa = saborDaMassa
         this.recheio = recheio
     }
+    // para declarar um funcao dentro de uma classe nao preciso usar a palavra function.
+    escrever(){
+        console.log(`Um delicioso bolo de: ${this.saborDaMassa} com recheio de ${this.recheio}`);
+    }
 }
 
-let boloFesta = new formaDeBolo ("massa de chocolate", "recheio de nutella")
-console.log(boloFesta);
+let boloFesta = new formaDeBolo ("Chocolate", " Nutella")
+boloFesta.escrever()
