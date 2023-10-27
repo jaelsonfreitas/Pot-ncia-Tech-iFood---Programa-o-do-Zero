15b276c4cd232620e8ebd10e145a3268ff696778
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-26 21:05:40 -0300

    Criando objetos na pratica

---
+++ estruturas-de-dados-e-objetos/oque-sao-classes-e-objetos/script.js
@@ -0,0 +1,9 @@
+class formaDeBolo {
+    constructor(saborDaMassa, recheio){
+        this.saborDaMassa = saborDaMassa
+        this.recheio = recheio
+    }
+}
+
+let boloFesta = new formaDeBolo ("massa de chocolate", "recheio de nutella")
+console.log(boloFesta);
