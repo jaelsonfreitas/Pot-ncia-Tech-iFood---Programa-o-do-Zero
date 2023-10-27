ba0c200f5cda59ba6f7d81cd87c1a2a7ead21083
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-26 22:48:23 -0300

    Definindo a classe e o metodo contructo.

---
+++ estruturas-de-dados-e-objetos/desafio-de-codigo/script.js
@@ -0,0 +1,8 @@
+// Definindo a classe Heroi
+class Heroi {
+    // Construtor da classe para criar objetos Heroi
+    constructor(nome, idade, tipo) {
+        this.nome = nome;   // Define a propriedade "nome" do objeto Heroi
+        this.idade = idade; // Define a propriedade "idade" do objeto Heroi
+        this.tipo = tipo;   // Define a propriedade "tipo" do objeto Heroi
+    }
