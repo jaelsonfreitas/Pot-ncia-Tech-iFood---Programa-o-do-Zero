e1da064614d857210fe387edda286096bddc8aa4
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-10-29 12:17:44 -0300

    Finalizando Curso.

--- introducao-a-logica-de-programacao/trabalhando-com-variaveis/src/exercicio.js
+++
@@ -1,8 +0,0 @@
-// nome, idade, numero de telefone, cpf, endereço.
-
-let nome = "Palmira Fidelis"
-let idade = 79
-let numTelefone = "(47)99237-8581"
-let cpf = '070.009.009-09'
-let endereco = "Rua do Sol, 777, Bairro do Sete Saltos, Ponta Grossa - PR. "
-let recebePensao = true
